bcdb5674550e3e59286b1e326c3bc2ef97b82dcc
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-10-26 02:57:29 -0300

    Create function to call to API, get values of currencies and save it in an array

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ let requestOptions = {
   headers: myHeaders
 };
 
+function getValuesEqualsOfCurrencies() {
+  fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2C%20EUR%2C%20JPY%2C%20GBP.%20AUD.%20CAD%2C%20CHF%2C%20CNY%2CSEK%2C%20MXN%2C%20NZD%2C%20SGD%2C%20HKD%2C%20NOK%2C%20KRW%2C%20TRY%2C%20INR%2C%20RUB%2C%20BRL&base=${currencyBase}`, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    arrayValues = Object.values(result.rates)
+  })
+  .catch(error => console.log('error', error));
+}
+
