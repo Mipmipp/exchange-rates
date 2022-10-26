1c253d330dd3a17981d433a880ac3aaf084178d5
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-10-26 03:06:19 -0300

    Create a function to request to the API every currency base

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ let requestOptions = {
   headers: myHeaders
 };
 
+
+function getCurrenciesList() {
+  fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+  .then(response => response.json())
+  .then(response => getEveryCurrency(response.symbols))
+  .catch(error => console.log('error', error));
+}
+
 function getBaseEqualsOfCurrencies() {
   fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2C%20EUR%2C%20JPY%2C%20GBP.%20AUD.%20CAD%2C%20CHF%2C%20CNY%2CSEK%2C%20MXN%2C%20NZD%2C%20SGD%2C%20HKD%2C%20NOK%2C%20KRW%2C%20TRY%2C%20INR%2C%20RUB%2C%20BRL&base=EUR", requestOptions)
   .then(response => response.json())
