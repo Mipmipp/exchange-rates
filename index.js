bf849323d45bc6cf542d8e986f70fee3bb4b622d
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-10-26 03:09:07 -0300

    Create a function to show new result and change text's symbol

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ function convertCurrencyToOther() {
   .catch(error => console.log('error', error));
 }
 
+function updateResult(result) {
+  const $result = document.querySelector('#result');
+  $result.value = result;
+  const $containerCurrencies = document.querySelector('#container-currencies');
+  const $containerCurrenciesText = document.querySelector('#text');
+  $containerCurrenciesText.textContent = `Did you know that 1 ${currencyBase} is equivalent to these currencies?`;
+
+}
+
 function getBaseEqualsOfCurrencies() {
   fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2C%20EUR%2C%20JPY%2C%20GBP.%20AUD.%20CAD%2C%20CHF%2C%20CNY%2CSEK%2C%20MXN%2C%20NZD%2C%20SGD%2C%20HKD%2C%20NOK%2C%20KRW%2C%20TRY%2C%20INR%2C%20RUB%2C%20BRL&base=EUR", requestOptions)
   .then(response => response.json())
