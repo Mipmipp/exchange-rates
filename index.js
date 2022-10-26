9e6f9bea64a662f3197613360045a3da7a7c45f7
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-10-26 02:56:12 -0300

    Create request options to call API

---
+++ index.js
@@ -0,0 +1,9 @@
+let myHeaders = new Headers();
+myHeaders.append("apikey", "9EbuNKqViyzPNFQDaqQRO509ta8vnRZM");
+
+let requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
